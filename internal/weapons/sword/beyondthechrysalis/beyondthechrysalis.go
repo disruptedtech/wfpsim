c52eb69f5e0d8ba6f78951d7cabb21f20a7374e4
disruptedtech <139924533+disruptedtech@users.noreply.github.com>
2026-09-20 21:51:02 +0200

    Update beyondthechrysalis.go

--- internal/weapons/sword/beyondthechrysalis/beyondthechrysalis.go
+++ internal/weapons/sword/beyondthechrysalis/beyondthechrysalis.go
@@ -1,6 +1,8 @@
 package beyondthechrysalis
 
 import (
+	"fmt"
+
 	"github.com/genshinsim/gcsim/pkg/core"
 	"github.com/genshinsim/gcsim/pkg/core/attributes"
 	"github.com/genshinsim/gcsim/pkg/core/event"
@@ -11,11 +13,10 @@ import (
 )
 
 const (
-	devotionKey    = "beyondthechrysalis-devotion"
-	defianceKey    = "beyondthechrysalis-defiance"
-	plentyKey      = "beyondthechrysalis-plenty"
-	buffDuration   = 10 * 60
-	energyInterval = 4 * 60
+	devotionKey  = "beyondthechrysalis-devotion"
+	defianceKey  = "beyondthechrysalis-defiance"
+	plentyKey    = "beyondthechrysalis-plenty"
+	buffDuration = 10 * 60
 )
 
 func init() {
@@ -49,66 +50,45 @@ func NewWeapon(c *core.Core, char *character.CharWrapper, p info.WeaponProfile) 
 	// Max energy per refine: 5/5.5/6/6.5/7
 	maxEnergy := 4.5 + float64(w.refine)*0.5
 
-	c.Events.Subscribe(event.OnSkill, func(args ...interface{}) bool {
-		return w.onSkillOrBurst(cdBuff, swirlBuff, maxEnergy)
-	}, "beyondthechrysalis-skill")
+	onSkillOrBurst := func(args ...any) {
+		if c.Player.Active() != char.Index() {
+			return
+		}
+		switch w.sequence % 3 {
+		case 0: // Winds of Devotion - CRIT DMG buff
+			m := make([]float64, attributes.EndStatType)
+			m[attributes.CD] = cdBuff
+			char.AddStatMod(character.StatMod{
+				Base:         modifier.NewBase(devotionKey, buffDuration),
+				AffectedStat: attributes.CD,
+				Amount: func() []float64 {
+					return m
+				},
+			})
+		case 1: // Winds of Defiance - Swirl DMG buff
+			m := make([]float64, attributes.EndStatType)
+			m[attributes.AnemoP] = swirlBuff
+			char.AddStatMod(character.StatMod{
+				Base:         modifier.NewBase(defianceKey, buffDuration),
+				AffectedStat: attributes.AnemoP,
+				Amount: func() []float64 {
+					return m
+				},
+			})
+		case 2: // Winds of Plenty - Energy regen
+			char.AddEnergy("beyondthechrysalis-plenty", maxEnergy)
+		}
+		w.sequence++
+	}
 
-	c.Events.Subscribe(event.OnBurst, func(args ...interface{}) bool {
-		return w.onSkillOrBurst(cdBuff, swirlBuff, maxEnergy)
-	}, "beyondthechrysalis-burst")
-
-	c.Events.Subscribe(event.OnCharacterSwap, func(args ...interface{}) bool {
+	c.Events.Subscribe(event.OnSkill, onSkillOrBurst, fmt.Sprintf("beyondthechrysalis-%v", char.Base.Key.String()))
+	c.Events.Subscribe(event.OnBurst, onSkillOrBurst, fmt.Sprintf("beyondthechrysalis-%v", char.Base.Key.String()))
+	c.Events.Subscribe(event.OnCharacterSwap, func(args ...any) {
 		prev := args[0].(int)
-		if prev == w.char.Index {
+		if prev == char.Index() {
 			w.sequence = 0
 		}
-		return false
-	}, "beyondthechrysalis-swap")
+	}, fmt.Sprintf("beyondthechrysalis-swap-%v", char.Base.Key.String()))
 
 	return w, nil
-}
-
-func (w *Weapon) onSkillOrBurst(cdBuff, swirlBuff, maxEnergy float64) bool {
-	if w.core.Player.Active() != w.char.Index {
-		return false
-	}
-
-	switch w.sequence % 3 {
-	case 0: // Winds of Devotion - CRIT DMG buff
-		val := make([]float64, attributes.EndStatType)
-		val[attributes.CD] = cdBuff
-		w.char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBase(devotionKey, buffDuration),
-			AffectedStat: attributes.CD,
-			Amount: func() ([]float64, bool) {
-				return val, true
-			},
-		})
-
-	case 1: // Winds of Defiance - Swirl DMG buff
-		val := make([]float64, attributes.EndStatType)
-		val[attributes.ElectroP] = swirlBuff
-		w.char.AddStatMod(character.StatMod{
-			Base:         modifier.NewBase(defianceKey, buffDuration),
-			AffectedStat: attributes.ElectroP,
-			Amount: func() ([]float64, bool) {
-				return val, true
-			},
-		})
-
-	case 2: // Winds of Plenty - Energy regen
-		energyGiven := 0.0
-		w.char.AddStatus(plentyKey, buffDuration, true)
-		w.core.Events.Subscribe(event.OnTick, func(args ...interface{}) bool {
-			if !w.char.StatusIsActive(plentyKey) || energyGiven >= maxEnergy {
-				return true
-			}
-			w.char.AddEnergy("beyondthechrysalis-plenty", maxEnergy-energyGiven)
-			energyGiven = maxEnergy
-			return false
-		}, plentyKey)
-	}
-
-	w.sequence++
-	return false
 }
